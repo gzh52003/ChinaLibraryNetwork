0d33778ea47071f256652f998bbac0f63a6b43e0
李明珍 <1746934940@qq.com>
2020-08-20 16:37:40 +0800

    添加用户信息

--- libraryWeb/src/router/index.js
+++ libraryWeb/src/router/index.js
@@ -5,22 +5,26 @@ import VueRouter from 'vue-router'
 
 
 import Home from '../pages/Home.vue'
-
 import User from '../pages/user/Default.vue'
 import UserList from '../pages/user/List.vue'
 import UserAdd from '../pages/user/Add.vue'
 import UserEdit from '../pages/user/Edit.vue'
-
-import Order from '../pages/Order.vue'
-import Goods from '../pages/Goods.vue'
+import Power from '../pages/power/Default.vue'
+import PowerList from '../pages/power/List.vue'
+import PowerRoles from '../pages/power/Roles.vue'
+import Order from '../pages/order/Default.vue'
+import OrderList from '../pages/order/List.vue'
+import Goods from '../pages/goods/Default.vue'
+import GoodsList from '../pages/goods/List.vue'
+import GoodsAdd from '../pages/goods/Add.vue'
 import Reg from '../pages/Reg.vue'
 import Login from '../pages/Login.vue'
 import NotFound from '../pages/NotFound.vue'
 
 
+
 // 2. 使用VueRouter
 Vue.use(VueRouter)
-
 // 3. 实例化并配置参数
 const router = new VueRouter({
     // mode:'history', // 一般上线后改为history路由(需要额外配置服务器)
@@ -54,12 +58,51 @@ const router = new VueRouter({
                 }]
         },
         {
+            path: '/power',
+            component: Power,
+            children: [
+                // 进入用户管理页面直接跳到用户列表
+                {
+                    path: '',
+                    redirect: 'list'
+                }, {
+                    path: 'list',
+                    component: PowerList
+                }, {
+                    path: 'roles',
+                    component:  PowerRoles
+                }]
+            
+        },
+        {
             path: '/order',
-            component: Order
+            component: Order,
+            children: [
+                // 进入用户管理页面直接跳到用户列表
+                {
+                    path: '',
+                    redirect: 'list'
+                }, {
+                    path: 'list',
+                    component: OrderList
+                }]
         },
         {
             path: '/goods',
-            component: Goods
+            component: Goods,
+            children: [
+                // 进入用户管理页面直接跳到用户列表
+                {
+                    path: '',
+                    redirect: 'list'
+                }, {
+                    path: 'add',
+                    component: GoodsAdd
+                }, {
+                    path: 'list',
+                    component: GoodsList
+                }]
+            
         },
         {
             path: '/login',
@@ -73,7 +116,6 @@ const router = new VueRouter({
             path: '/404',
             component: NotFound
         },
-
         // 404页面效果
         {
             path: '*',
